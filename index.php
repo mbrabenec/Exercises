03f8530fe54d90d617b699a84e3b8e5a5ae55361
mbrabenec <brabzzz@gmail.com>
2020-12-27 23:48:33 +0100

    ul

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
 <?php
-
-
-$favs = array("fruit"=>"banana","veggie"=>"carrot","meat"
-=>"roast beef"); 
-
-foreach($favs as $food => $type) {
-echo "$food - $type\n"; 
+for ($i = 0; $i < 10; $i++) {
+    echo $i;
 }
+
+
+?>
+
+
+</body>
+</html>
 
